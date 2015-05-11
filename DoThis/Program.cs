be5d3c074b4edd853b24469ace4d118854d90bdd
Jens Pettersson <jens.pettersson@live.se>
2015-05-11 15:01:27 +0200

    Completed lesson 4

--- DoThis/Program.cs
+++ DoThis/Program.cs
@@ -11,14 +11,17 @@ namespace WinTail
             MyActorSystem = ActorSystem.Create("MyActorSystem");
 
             Props consoleWriterProps = Props.Create<ConsoleWriterActor>();
-
             var consoleWriterActor = MyActorSystem.ActorOf(consoleWriterProps, "consoleWriterActor");
 
-            Props validationProps = Props.Create(() => new ValidationActor(consoleWriterActor));
+            var tailCoordinatorProps = Props.Create(() => new TailCoordinatorActor());
+            var tailCoordinatorActor = MyActorSystem.ActorOf(tailCoordinatorProps, "tailCoordinatorActor");
 
-            var validationActor = MyActorSystem.ActorOf(validationProps, "validationActor");
 
-            Props consoleReaderProps = Props.Create(() => new ConsoleReaderActor(validationActor));
+            Props fileValidationProps = Props.Create(() => new FileValidationActor(consoleWriterActor, tailCoordinatorActor));
+
+            var fileValidationActor = MyActorSystem.ActorOf(fileValidationProps, "fileValidationActor");
+
+            Props consoleReaderProps = Props.Create(() => new ConsoleReaderActor(fileValidationActor));
 
             var consoleReaderActor = MyActorSystem.ActorOf(consoleReaderProps, "consoleReaderActor");
 
---
+++ DoThis/TailCoordinatorActor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Akka.Actor;
+
+namespace WinTail
+{
+    public class TailCoordinatorActor : UntypedActor
+    {
+        public class StartTail
+        {
+            public string FilePath { get; private set; }
+            public IActorRef ReporterActor { get; private set; }
+
+            public StartTail(string filePath, IActorRef reporterActor)
+            {
+                FilePath = filePath;
+                ReporterActor = reporterActor;
+            }
+        }
+
+        public class StopStail
+        {
+            public string FilePath { get; private set; }
+
+            public StopStail(string filePath)
+            {
+                FilePath = filePath;
+            }
+        }
+
+        protected override void OnReceive(object message)
+        {
+            if (message is StartTail)
+            {
+                var msg = message as StartTail;
+
+                Context.ActorOf(Props.Create(() => new TailActor(msg.ReporterActor, msg.FilePath)));
+            }
+        }
+
+        protected override SupervisorStrategy SupervisorStrategy()
+        {
+            return new OneForOneStrategy(
+                maxNrOfRetries: 10,
+                withinTimeRange: TimeSpan.FromSeconds(30),
+                localOnlyDecider: x =>
+                {
+                    if(x is ArithmeticException)
+                        return Directive.Resume;
+                    if(x is NotSupportedException)
+                        return Directive.Stop;
+                    
+                    return Directive.Restart;
+                });
+        }
+    }
+}
